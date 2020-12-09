a1fe88dd529d08c74dd82e34968e585328b2d06d
haru-arai <h.arai@tir-group.jp>
2020-12-09 16:11:42 +0900

    ログイン画面作成

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@sh
 Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
-Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 
 Auth::routes();
 
